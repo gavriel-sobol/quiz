89628fb1a0113d81b458bf751c399deb3cdb33e2
Sobol Oleg <gavriel.sobol@gmail.com>
2017-03-05 23:23:54 +0200

    ver0.1

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,17 @@
-(function ($){
-    $('li').click(function(){
+ $(document).ready(function(){
+    $('li').click(function(e){
+       var div__id=document.getElementById($(this).data('id'));
         $(this).toggleClass('visiblenone');
         $(this).parent().parent().parent().toggleClass('block-question--spryatant');
-        $("#my").toggleClass('block-content--spryatant');
+        $(div__id).toggleClass('block-content--spryatant');
     });
-})(jQuery);
+    
+    $('.block-content').click(function(e){
+         $('.block-question').toggleClass('block-question--spryatant');
+         $(this).toggleClass('block-content--spryatant');
+    });
+
+})
+(jQuery);
 
  
