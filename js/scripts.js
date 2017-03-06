98702d56d904dd12c93593d4a493357328f81fec
Sobol Oleg <gavriel.sobol@gmail.com>
2017-03-07 00:27:34 +0200

    Change style

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,2 @@
- $(document).ready(function(){
-    $('li').click(function(e){
-       var div__id=document.getElementById($(this).data('id'));
-        $(this).toggleClass('visiblenone');
-        $(this).parent().parent().parent().toggleClass('block-question--spryatant');
-        $(div__id).toggleClass('block-content--spryatant');
-    });
-    
-    $('.block-content').click(function(e){
-         $('.block-question').toggleClass('block-question--spryatant');
-         $(this).toggleClass('block-content--spryatant');
-    });
-
-})
-(jQuery);
-
+ 
  
