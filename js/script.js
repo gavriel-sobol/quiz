8dc0539f85d4b5b2172b46d2ea1744760933760a
Sobol Oleg <gavriel.sobol@gmail.com>
2017-03-16 17:33:00 +0200

    16.03.17:17.32

--- js/script.js
+++ js/script.js
@@ -2,14 +2,24 @@
     var link = $('.nav__item'),
         slideItem = $('.slide__item');
         activeClass= 'active';
-
         link.on('click', activateSlide);
-
-        function activateSlide(e) {
+     function activateSlide(e) {
             e.preventDefault();
             var index = $(this).index();
-
             slideItem.parent().find('.slide__item' + '.' + activeClass).removeClass(activeClass);
             slideItem.eq(index).addClass(activeClass);
-        }
-})(jQuery);
+            
+    }
+  
+})
+(jQuery);
+
+
+ $('.slide__arrow-button').click(function(e){
+            $(this).toggleClass('slide__arrow-active');
+            $('.slide__sidebar').toggleClass('slide__sidebar-active');
+})
+
+
+            
+(jQuery);
